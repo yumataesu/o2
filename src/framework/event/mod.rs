1f806b142f1a67c51a0d6250e12dadcf59b23736
YumaTaesu <yuma.taesu@gmail.com>
2021-04-15 10:44:52 +0900

    update : moduled

---
+++ src/framework/event/mod.rs
@@ -0,0 +1,81 @@
+use std::sync::{Arc, Mutex};
+
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    ErrorA, ErrorB
+}
+
+pub type GameResult<T = ()> = Result<T, Error>;
+
+#[derive(Default)]
+pub struct EventSystem {
+    wrapped_observers: Vec<Arc<Mutex<dyn Observer>>>,
+}
+
+impl EventSystem {
+    pub fn new() -> EventSystem {
+        EventSystem {
+            wrapped_observers: vec![],
+        }
+    }
+
+    pub fn notify(&self, event: Event) {
+        for wrapped_observer in self.wrapped_observers.clone() {
+            let mut observer = wrapped_observer.lock().unwrap();
+            observer.on_notify(&event);
+        }
+    }
+
+    pub fn add_observer(&mut self, observer: Arc<Mutex<dyn Observer>>) {
+        self.wrapped_observers.push(observer);
+    }
+}
+
+
+pub trait Observer {
+    fn on_notify(&mut self, event: &Event);
+}
+
+
+pub enum Event {
+    MouseEvent,
+    KeyEvent,
+    ResizeEvent,
+    DropEvent
+}
+
+#[derive(Default)]
+pub struct MouseEventArgs {
+    mouse_button: u8,
+    mouse_position: glam::Vec2
+}
+
+impl MouseEventArgs {
+    pub fn new() -> GameResult<Arc<Mutex<MouseEventArgs>>> {
+        Ok(Arc::new(Mutex::new(MouseEventArgs{ mouse_button:0, mouse_position: glam::Vec2::new(0.0, 0.0) })))
+    }
+}
+
+impl Observer for MouseEventArgs {
+    fn on_notify(&mut self, event: &Event) {
+        match event {
+            Event::MouseEvent => {
+                println!("MouseEvent");
+                // self.player += 1;
+            },
+            Event::KeyEvent => {
+                println!("KeyEvent");
+                // self.ai += 1;
+            }
+            Event::ResizeEvent => {
+                println!("ResizeEvent");
+                // self.ai += 1;
+            },
+            Event::DropEvent => {
+                println!("DropEvent");
+                // self.ai += 1;
+            }
+        }
+    }
+}
--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -1,9 +1,9 @@
 extern crate glam;
 mod opengl;
 mod macros;
+mod event;
 
 use gl;
-use std::sync::{Arc, Mutex};
 
 pub use imgui_glfw_rs::glfw::{self, Context};
 use imgui_glfw_rs::imgui;
@@ -22,88 +22,11 @@ pub use opengl::Texture as Texture;
 pub use opengl::Utils as gl_utils;
 pub use opengl::WindowSettings as WindowSettings;
 
+pub use event::EventSystem as EventSystem;
+pub use event::MouseEventArgs as MouseEventArgs;
+pub use event::Event as Event;
+
 use crate::app::App;
-
-
-#[derive(Debug, Clone)]
-pub enum Error {
-    ErrorA, ErrorB
-}
-
-pub type GameResult<T = ()> = Result<T, Error>;
-
-#[derive(Default)]
-pub struct EventSystem {
-    wrapped_observers: Vec<Arc<Mutex<dyn Observer>>>,
-}
-
-impl EventSystem {
-    pub fn new() -> EventSystem {
-        EventSystem {
-            wrapped_observers: vec![],
-        }
-    }
-
-    pub fn notify(&self, event: Event) {
-        for wrapped_observer in self.wrapped_observers.clone() {
-            let mut observer = wrapped_observer.lock().unwrap();
-            observer.on_notify(&event);
-        }
-    }
-
-    pub fn add_observer(&mut self, observer: Arc<Mutex<dyn Observer>>) {
-        self.wrapped_observers.push(observer);
-    }
-}
-
-
-pub trait Observer {
-    fn on_notify(&mut self, event: &Event);
-}
-
-
-pub enum Event {
-    MouseEvent,
-    KeyEvent,
-    ResizeEvent,
-    DropEvent
-}
-
-#[derive(Default)]
-pub struct MouseEventArgs {
-    mouse_button: u8,
-    mouse_position: glam::Vec2
-}
-
-impl MouseEventArgs {
-    pub fn new() -> GameResult<Arc<Mutex<MouseEventArgs>>> {
-        Ok(Arc::new(Mutex::new(MouseEventArgs{ mouse_button:0, mouse_position: glam::Vec2::new(0.0, 0.0) })))
-    }
-}
-
-impl Observer for MouseEventArgs {
-    fn on_notify(&mut self, event: &Event) {
-        match event {
-            Event::MouseEvent => {
-                println!("MouseEvent");
-                // self.player += 1;
-            },
-            Event::KeyEvent => {
-                println!("KeyEvent");
-                // self.ai += 1;
-            }
-            Event::ResizeEvent => {
-                println!("ResizeEvent");
-                // self.ai += 1;
-            },
-            Event::DropEvent => {
-                println!("DropEvent");
-                // self.ai += 1;
-            }
-        }
-    }
-}
-
 
 pub trait BaseApp {
     fn setup(&mut self);
